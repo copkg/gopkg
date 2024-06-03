84a43339f02498e509f98bb528ace200a8ac77cf
amei <415984085@qq.com>
2024-06-03 09:31:31 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/copkg/gopkg
+
+go 1.21.4
---
+++ schema/customer.go
@@ -0,0 +1,9 @@
+package schema
+
+type CustomerListRequest struct {
+	A int64
+	B int64
+}
+type CustomerListResponse struct {
+	C int64
+}
